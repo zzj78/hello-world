2ec4415462a96506622e8d7355541a9c4cfbaa7d
zzj78 <83437579+zzj78@users.noreply.github.com>
2021-09-02 14:51:19 +0800

    Create PID.C
    to be continue

---
+++ PID.C
@@ -0,0 +1,15 @@
+#include "stidio.h"
+#include "PID.h"
+
+void PIDController_Init(PIDController *pid) {
+
+	/* Clear controller variables */
+	pid->integrator = 0.0f;
+	pid->prevError  = 0.0f;
+
+	pid->differentiator  = 0.0f;
+	pid->prevMeasurement = 0.0f;
+
+	pid->out = 0.0f;
+
+}
